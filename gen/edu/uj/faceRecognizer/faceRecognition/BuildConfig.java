991bac8291ba3111cd8f4bc94e5afc955b9b8ece
piotrplaneta <piotr.planeta@gmail.com>
2013-01-15 21:54:22 +0100

    faceRecognition refined, changed app package

---
+++ gen/edu/uj/faceRecognizer/faceRecognition/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.uj.faceRecognizer.faceRecognition;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ gen/edu/uj/faceRecognizer/faceRecognition/R.java
@@ -0,0 +1,5 @@
+package edu.uj.faceRecognizer.faceRecognition;
+
+/* This stub is for using by IDE only. It is NOT the R class actually packed into APK */
+public final class R {
+}
