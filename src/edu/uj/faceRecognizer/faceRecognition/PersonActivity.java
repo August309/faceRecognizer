9d68d7f718399c51ed70e1e078b54bfd66e2cf24
mokrzu <mokrzu@gmail.com>
2013-01-21 17:14:29 +0100

    fixed photo capture & crop

--- src/edu/uj/faceRecognizer/faceRecognition/PersonActivity.java
+++ src/edu/uj/faceRecognizer/faceRecognition/PersonActivity.java
@@ -17,19 +17,21 @@ import android.widget.Toast;
 
 public class PersonActivity extends Activity {
 	
-    private int counter;
+    private static int counter;
     private static String personName;
     private static final int CAMERA_PIC_REQUEST = 1337;
     private static final int CROP_REQ = 1333;
     private File storageDir;
     private static File[] takenPhotoFiles;
+    private static int restore;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.person_activity2);
-        counter = 0;
+        counter = -1;
         takenPhotoFiles = new File[3];
+        restore = 0;
 	}
 	
     @Override
@@ -49,12 +51,18 @@ public class PersonActivity extends Activity {
        
 	        try {
 	        	
-	        	System.out.println("B: " + takenPhotoFiles[counter]);
-	        	takenPhotoFiles[counter] = createImageFile();
-	        	System.out.println("A: " + takenPhotoFiles[counter]);
+	        	counter = counter + 1;
 	        	
-				System.out.println(takenPhotoFiles[counter].getAbsolutePath());
+
+	        	takenPhotoFiles[counter] = createImageFile();
+
+	        	
+				System.out.println("Counter ====> " + counter);
+				for (File f : takenPhotoFiles) {
+                	System.out.println("FILE ---> " + f);	
+                }
 				cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(takenPhotoFiles[counter]));
+				cameraIntent.putExtra("idx", counter);
 				
 				startActivityForResult(cameraIntent, CAMERA_PIC_REQUEST);
 				
@@ -71,43 +79,27 @@ public class PersonActivity extends Activity {
         if (requestCode == CAMERA_PIC_REQUEST) {
             if(resultCode == Activity.RESULT_OK) {
                 System.out.println("CHECKED!");
-                counter += 1;
-	                
-	             if (counter < 2) {
-                	CharSequence text = "Fine, take another!";
-                    Toast toast = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT);
-                    toast.show();
-                    
-  
-                } else {
-                	CharSequence text = "Photos captured & saved!";
-                    Toast toast = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT);
-                    toast.show();
-                    
-                    Intent intent = new Intent("com.android.camera.action.CROP");
-   	             
-                    for (File f : takenPhotoFiles) {
-			             // this will open all images in the Galery
-			               System.out.println(f.getAbsolutePath());
-			             intent.setDataAndType(Uri.fromFile(f), "image/*");
-			             intent.putExtra("crop", "true");
-			             // this defines the aspect ration
-			             intent.putExtra("aspectX", 4);
-			             intent.putExtra("aspectY", 3);
-			             // this defines the output bitmap size
-			             intent.putExtra("outputX", 200);
-			             intent.putExtra("outputY", 150);
-			             // true to return a Bitmap, false to directly save the cropped iamge
-			             intent.putExtra("return-data", false);
-			             //save output image in uri
-			             intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
-			             startActivity(intent);
-                    }
-                    
-                	Intent intentEnd = new Intent(this, MainActivity.class);
-                	startActivity(intentEnd);
-                	
-                }
+                
+                //System.out.println("EXTRA! " + data.getStringExtra("idx"));
+                
+	             
+            	Intent intent = new Intent("com.android.camera.action.CROP");
+	             //System.out.println(.getAbsolutePath());
+	             intent.setDataAndType(Uri.fromFile(takenPhotoFiles[restore]), "image/*");
+	             intent.putExtra("crop", "true");
+	             // this defines the aspect ration
+	             intent.putExtra("aspectX", 4);
+	             intent.putExtra("aspectY", 3);
+	             // this defines the output bitmap size
+	             intent.putExtra("outputX", 200);
+	             intent.putExtra("outputY", 150);
+	             // true to return a Bitmap, false to directly save the cropped iamge
+	             intent.putExtra("return-data", false);
+	             //save output image in uri
+	             intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(takenPhotoFiles[restore]));
+	             startActivity(intent);
+	             restore += 1;
+	            
                 
             } else {
             	takenPhotoFiles[counter].delete();
